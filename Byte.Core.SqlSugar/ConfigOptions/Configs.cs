61782e676c5e95cbb92ac5b2935daec665894ac4
熊玉麒 <617553471@qq.com>
2024-03-29 17:59:43 +0800

    11

---
+++ Byte.Core.SqlSugar/ConfigOptions/Configs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Byte.Core.SqlSugar.ConfigOptions;
+
+/// <summary>
+/// 全局配置类
+/// </summary>
+public class Configs
+{
+  
+
+    #region 数据库连接对象
+
+    private DbConnection _dataConnection;
+
+    /// <summary>
+    ///  数据库连接对象
+    /// </summary>
+    public DbConnection DataConnection
+    {
+        get
+        {
+            if (_dataConnection == null)
+            {
+                _dataConnection = new DbConnection
+                {
+                    ConnectionItem = new List<ConnectionItem>()
+                };
+            }
+
+            return _dataConnection;
+        }
+        set => _dataConnection = value;
+    }
+    #endregion
+}
---
+++ Byte.Core.SqlSugar/Repository/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using SqlSugar;
+
+namespace Byte.Core.SqlSugar.Repository;
+
+public interface IUnitOfWork
+{
+    SqlSugarScope GetDbClient();
+    void BeginTran();
+    void CommitTran();
+    void RollbackTran();
+}
