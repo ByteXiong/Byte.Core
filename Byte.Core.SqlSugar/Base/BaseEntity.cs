2174e9efdbd96979f6c35b620862b92680b63bf7
熊玉麒 <617553471@qq.com>
2024-05-24 15:43:03 +0800

    11

--- Byte.Core.SqlSugar/Base/BaseEntity.cs
+++ Byte.Core.SqlSugar/Base/BaseEntity.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using SqlSugar;
 
 namespace Byte.Core.SqlSugar.Base
@@ -18,17 +19,18 @@ namespace Byte.Core.SqlSugar.Base
         /// </summary>
         [SugarColumn(IsNullable = true)]
         public DateTime CreateTime { get; set; }
-
         /// <summary>
         /// 更新者名称
         /// </summary>
         [SugarColumn(IsNullable = true)]
+        [JsonIgnore]//隐藏
         public string UpdateBy { get; set; }
 
         /// <summary>
         /// 最后更新时间
         /// </summary>
         [SugarColumn(IsNullable = true)]
+        [JsonIgnore]//隐藏
         public DateTime? UpdateTime { get; set; }
     }
 }
