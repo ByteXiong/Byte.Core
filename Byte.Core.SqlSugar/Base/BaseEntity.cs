f262acacb1d4a62486eff3543adac0c54d855b7a
围城 <617553471@qq.com>
2024-12-03 17:44:43 +0800

    接口日志

--- Byte.Core.SqlSugar/Base/BaseEntity.cs
+++ Byte.Core.SqlSugar/Base/BaseEntity.cs
@@ -17,8 +17,8 @@ namespace Byte.Core.SqlSugar
         /// <summary>
         /// 创建时间
         /// </summary>
-        [SugarColumn(InsertServerTime = true, IsNullable = true)]
-        public DateTime CreateTime { get; set; }
+        [SugarColumn( IsNullable = true)]
+        public long CreateTime { get; set; }
         /// <summary>
         /// 更新者名称
         /// </summary>
@@ -28,7 +28,7 @@ namespace Byte.Core.SqlSugar
         /// <summary>
         /// 最后更新时间
         /// </summary>
-        [SugarColumn(IsOnlyIgnoreUpdate = true,IsNullable = true)]
-        public DateTime? UpdateTime { get; set; }
+        [SugarColumn(IsNullable = true)]
+        public long? UpdateTime { get; set; }
     }
 }
