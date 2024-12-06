1f9e37d696992ceb03a39b1e0052048eb1648bba
围城 <617553471@qq.com>
2024-12-06 18:22:57 +0800

    添加 修改密码

---
+++ Byte.Core.Common/Attributes/NoCheckRoleAttribute.cs
@@ -0,0 +1,15 @@
+namespace Byte.Core.Common.Attributes
+{
+    /// <summary>
+    /// 忽略权限
+    /// </summary>
+    public class NoCheckRoleAttribute : BaseActionFilter
+    {
+    }
+    /// <summary>
+    /// Get鉴权
+    /// </summary>
+    public class GetCheckRoleAttribute : BaseActionFilter
+    {
+    }
+}
